2d51e0e3dc549a3d86b466b2cc62feaf8c0b7d47
maykhaing-saltnpixel <maykhaing@saltnpixel.com>
2023-03-15 08:09:36 +0000

    server

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInit82f16a42032adc04aaa803c3fa7b6a1e
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit82f16a42032adc04aaa803c3fa7b6a1e', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit82f16a42032adc04aaa803c3fa7b6a1e', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit82f16a42032adc04aaa803c3fa7b6a1e::getInitializer($loader));
         } else {
