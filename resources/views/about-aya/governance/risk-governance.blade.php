7223ee3893a50221fdd93032505b6ed57635ab85
maykhaing-saltnpixel <maykhaing@saltnpixel.com>
2023-06-08 16:15:28 +0630

    mk

--- resources/views/about-aya/governance/risk-governance.blade.php
+++ resources/views/about-aya/governance/risk-governance.blade.php
@@ -37,66 +37,31 @@
 										<span class="accordion-close">
 											<i class="menu-icon tf-icons bx bx-minus"></i>
 										</span>
-										Margin Financing (Letter of Credit)
+										Board Risk and Compliance Committee (BRCC)
 									</button>
 								</h2>
 								<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
 									<div class="accordion-body">
-										<div class="col-md-12 margin-financing-letter-of-credit">
-											<p class="margin_financing_tagline">Enlarge your investment power with our LC Margin Financing</p>
-										</div>
-										<div class="space-30"></div>
-										<p>We strategically provide financing for importers to cover their credit risk on a temporary basis. Through this type of collateral, we allow importers to expand investment by leveraging their assets. The LC Margin Financing product is where LC is issued for less than cash margin collateralized for your import business.</p>
-										<div class="space-20"></div>
-										<p class="fw-semibold" style="font-size: 17px;">Features and Benefits</p>
-
-										<table class="table table-borderless" cellspacing="0">
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/convenient-line-of-credit-for-issuing-letter-of-credit.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p>A convenient line of credit for issuing letter of credit</p>
-												</td>
-											</tr>
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/leverage-the-value-of-assets.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p class="w-80">Able to leverage the value of assets you already own to increase the size of your working capital</p>
-												</td>
-											</tr>
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/flexible-repayment-and-on-your-own-schedule.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p class="w-80">Flexible repayment and on your own schedule as long as your outstanding debt is within margin maintenance requirement</p>
-												</td>
-											</tr>
-										</table>
-
-										<div class="space-20"></div>
-										<p class="fw-semibold mb-30" style="font-size: 17px;">Eligibility</p>
 										<div class="row">
-											<div class="col-1">
-												<i class="menu-icon tf-icons bx bx-check secure_req_application_icon"></i>
+											<p>The Audit, Risk and Compliance Committee (ARCC) was established in 2014. In 2018, it was elevated to the Board level and split into two separate entities: the Audit Committee (AC) and the Board Risk and Compliance Committee (BRCC). The BRCC aids the Board in meeting its supervisory responsibilities concerning risk management, compliance, and the preservation of ethics and integrity in compliance with current laws and regulations. In fulfilling its oversight role, the Committee carries out the tasks and responsibilities defined in the Risk and Compliance Committee Charter.</p>
+											<p>The Board Risk and Compliance Committee is entrusted with the following duties:</p>
+											<div class="col-1 risk_governance_col_1">
+												<img src="{{ url('/images/bullet_square.jpg') }}" class="img-fluid bullet_square">
 											</div>
-											<div class="col-11">
-												<p class="margin_financing_eligibility mt-minus-5">Finance for marketable or non-perishable goods</p>
+											<div class="col-11 risk_governance_col_11">
+												<p>Developing policies tied to risk management that align with the strategic direction and overall risk appetite as determined by the Board of Directors.</p>
 											</div>
-											<div class="col-1">
-												<i class="menu-icon tf-icons bx bx-check secure_req_application_icon"></i>
+											<div class="col-1 risk_governance_col_1">
+												<img src="{{ url('/images/bullet_square.jpg') }}" class="img-fluid bullet_square">
 											</div>
-											<div class="col-11">
-												<p class="margin_financing_eligibility mt-minus-5">Goods must be not subject to price volatility</p>
+											<div class="col-11 risk_governance_col_11">
+												<p>Guaranteeing that the Senior Management abides by the risk tolerance boundaries set and suggesting adjustments as necessary in response to significant shifts in the internal and external environment.</p>
 											</div>
-											<div class="col-1">
-												<i class="menu-icon tf-icons bx bx-check secure_req_application_icon"></i>
+											<div class="col-1 risk_governance_col_1">
+												<img src="{{ url('/images/bullet_square.jpg') }}" class="img-fluid bullet_square">
 											</div>
-											<div class="col-11">
-												<p class="margin_financing_eligibility mt-minus-5">Importers buying on sight or credit terms are required to use LC as a settlement method for sales agreements</p>
+											<div class="col-11 risk_governance_col_11">
+												<p>Consistently supervising and evaluating the effectiveness of the Risk Management Policy.</p>
 											</div>
 										</div>
 									</div>
@@ -111,136 +76,49 @@
 										<span class="accordion-close">
 											<i class="menu-icon tf-icons bx bx-minus"></i>
 										</span>
-										Trust Receipt
+										Enterprise Risk Management Department 
 									</button>
 								</h2>
 								<div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
 									<div class="accordion-body">
-										<div class="col-md-12 trust-receipt">
-											<p class="trust_receipt_tagline">Invest more in working capital to achieve tangible profits</p>
-										</div>
-										<div class="space-30"></div>
-										<p>We provide advances for settlement of bills drawn by the buyer for the goods purchased under letter of credit. A credit trade line is required before utilizing the facility.  Through the trust receipt, importers can release the goods against their undertaking to sell and the proceeds can be used to pay off the loan. <span style="font-size: 13px;">Importers can enjoy the longer tenor which granted by AYA (Repayment at TR’s maturity date)</span></p>
-										<div class="space-20"></div>
-										<p class="fw-semibold" style="font-size: 17px;">Features and Benefits</p>
-
-										<table class="table table-borderless" cellspacing="0">
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/trust_receipt_benefit_1.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p>Help with liquidity by financing customer’s international payables, hence increasing confidence between importer and seller</p>
-												</td>
-											</tr>
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/trust_receipt_benefit_2.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p class="w-80">A sound choice for importers with insufficient cash flow</p>
-												</td>
-											</tr>
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/trust_receipt_benefit_3.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p class="w-80">Paying back loans only after goods are sold</p>
-												</td>
-											</tr>
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/trust_receipt_benefit_4.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p class="w-80">Flexible repayment plan</p>
-												</td>
-											</tr>
-										</table>
-
-										<div class="space-20"></div>
-										<p class="fw-semibold mb-30" style="font-size: 17px;">Eligibility</p>
 										<div class="row">
-											<div class="col-1">
-												<i class="menu-icon tf-icons bx bx-check secure_req_application_icon"></i>
+											<p>The Enterprise Risk Management Department holds the responsibility for the oversight of AYA Bank's Enterprise Risk Management (ERM) framework and provides counsel on risk management across all branches and the entirety of the bank's business operations.</p>
+											<p>The obligations and duties of the Enterprise Risk Management Department include:</p>
+											<div class="col-1 risk_governance_col_1">
+												<img src="{{ url('/images/bullet_square.jpg') }}" class="img-fluid bullet_square">
 											</div>
-											<div class="col-11">
-												<p class="margin_financing_eligibility mt-minus-5">Importers buying on sight or credit terms whereby sales agreement required use of LC as settlement method</p>
+											<div class="col-11 risk_governance_col_11">
+												<p>Recognizing and managing potential risk zones to safeguard the bank's and its shareholders' assets,</p>
 											</div>
-											<div class="col-1">
-												<i class="menu-icon tf-icons bx bx-check secure_req_application_icon"></i>
+											<div class="col-1 risk_governance_col_1">
+												<img src="{{ url('/images/bullet_square.jpg') }}" class="img-fluid bullet_square">
 											</div>
-											<div class="col-11">
-												<p class="margin_financing_eligibility mt-minus-5">Goods must be marketable, non-perishable, and not subject to price volatility.</p>
+											<div class="col-11 risk_governance_col_11">
+												<p>Evaluating if the correct risks have been identified and are being appropriately managed,</p>
 											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="accordion-item">
-								<h2 class="accordion-header b-b-none" id="headingThree">
-									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree" style="padding: 10px 0px 10px 20px !important;">
-										<span class="accordion-open">
-											<i class="menu-icon tf-icons bx bx-plus"></i>
-										</span>
-										<span class="accordion-close">
-											<i class="menu-icon tf-icons bx bx-minus"></i>
-										</span>
-										Import Invoice Financing
-									</button>
-								</h2>
-								<div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-									<div class="accordion-body">
-										<div class="col-md-12 import-invoice-financing">
-											<p class="import_invoice_financing_tagline">Avoid any credit and cash flow setbacks and instantly fulfill your business liquidity needs with our Invoice Financing</p>
-										</div>
-										<div class="space-30"></div>
-										<p>We support import invoice financing for instant coverage of your business needs associated with receiving goods for import.</p>
-										<div class="space-20"></div>
-										<p class="fw-semibold" style="font-size: 17px;">Features and Benefits</p>
-
-										<table class="table table-borderless" cellspacing="0">
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/import_invoice_financing_benefit_1.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p>More flexibility than business loans or overdrafts</p>
-												</td>
-											</tr>
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/import_invoice_financing_benefit_2.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p class="w-80">Payment is guaranteed to your exporter</p>
-												</td>
-											</tr>
-											<tr>
-												<td class="benefit_table_icon_td">
-													<img src="{{ url('/images/business/trade/trade-financing/import_invoice_financing_benefit_3.png') }}" class="img-fluid c-b-l-icon">
-												</td>
-												<td>
-													<p class="w-80">Maintain a smooth operation of the supply chain and improve cash flow</p>
-												</td>
-											</tr>
-										</table>
-
-										<div class="space-20"></div>
-										<p class="fw-semibold mb-30" style="font-size: 17px;">Eligibility</p>
-										<div class="row">
-											<div class="col-1">
-												<i class="menu-icon tf-icons bx bx-check secure_req_application_icon"></i>
+											<div class="col-1 risk_governance_col_1">
+												<img src="{{ url('/images/bullet_square.jpg') }}" class="img-fluid bullet_square">
 											</div>
-											<div class="col-11">
-												<p class="margin_financing_eligibility mt-minus-5">Goods must be merchandized goods, be marketable, non-perishable, saleable, and not subject to price volatility.</p>
+											<div class="col-11 risk_governance_col_11">
+												<p>Acknowledging the current areas of highest risk (both financial and non-financial) and regularly reviewing these sectors,</p>
 											</div>
-											<div class="col-1">
-												<i class="menu-icon tf-icons bx bx-check secure_req_application_icon"></i>
+											<div class="col-1 risk_governance_col_1">
+												<img src="{{ url('/images/bullet_square.jpg') }}" class="img-fluid bullet_square">
 											</div>
-											<div class="col-11">
-												<p class="margin_financing_eligibility mt-minus-5">Goods to be financed at the discretion of the Bank</p>
+											<div class="col-11 risk_governance_col_11">
+												<p>Receiving frequent updates from management, the Bank's legal team, auditors, and any necessary external parties concerning audit, financial reporting, and risk management.</p>
+											</div>
+											<div class="col-1 risk_governance_col_1">
+												<img src="{{ url('/images/bullet_square.jpg') }}" class="img-fluid bullet_square">
+											</div>
+											<div class="col-11 risk_governance_col_11">
+												<p>Assessing the adequacy of the management Information & Intelligence Systems for the effective management of the bank's customers and business – for instance, risks and profitability,</p>
+											</div>
+											<div class="col-1 risk_governance_col_1">
+												<img src="{{ url('/images/bullet_square.jpg') }}" class="img-fluid bullet_square">
+											</div>
+											<div class="col-11 risk_governance_col_11">
+												<p>Reviewing and tracking the effectiveness of the bank's controls concerning credit, operational, and financial risks.</p>
 											</div>
 										</div>
 									</div>
