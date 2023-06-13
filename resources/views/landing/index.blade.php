43882f89466bcc001daa574f41d11020e7b49718
maykhaing-saltnpixel <maykhaing@saltnpixel.com>
2023-06-13 16:35:39 +0630

    mk

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -20,7 +20,7 @@
 					<div class="carousel-inner active">
 						<div class="carousel-item carousel-item-1 active">
 							<video class="img-fluid xl_slide" autoplay loop muted plays-inline>
-								<source src="{{ url('/images/home/landing_xl.mp4') }}" type="video/mp4" />
+								<source src="{{ url('/images/home/landing_1920_600.webm') }}" type="video/webm" />
 							</video>
 							<video class="img-fluid desk_slide" autoplay loop muted plays-inline>
 								<source src="{{ url('/images/home/desk.mp4') }}" poster="{{ url('/images/home/desk_poster.jpeg') }}" type="video/mp4" />
