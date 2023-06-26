138ff2d12f4edb06efb6c101d018b89b0fcb5a25
maykhaing-saltnpixel <maykhaing@saltnpixel.com>
2023-06-26 11:16:49 +0630

    mk

--- resources/views/about-aya/news-room/corporate-detail.blade.php
+++ resources/views/about-aya/news-room/corporate-detail.blade.php
@@ -17,7 +17,7 @@
 				<div class="row">
 					<div class="col-md-9">
 						<div class="row">
-							<img src="{{ url($blog->featured_image) }}" class="img-fluid">
+							<img src="{{ url('/public/'.$blog->featured_image) }}" class="img-fluid">
 						</div>
 						<div class="space-40"></div>
 						<div class="row">
