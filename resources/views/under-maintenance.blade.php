ae35c3daf9689c252119605b713537bb54b5169e
maykhaing-saltnpixel <maykhaing@saltnpixel.com>
2023-06-26 18:18:50 +0630

    mk

--- resources/views/under-maintenance.blade.php
+++ resources/views/under-maintenance.blade.php
@@ -7,13 +7,15 @@
 <div class="container-xxl">
     <div class="misc-wrapper">
 		<div class="">
-          	<img src="{{ url('/images/under_maintenance.png') }}" alt="girl-doing-yoga-light" class="img-fluid" width="600" data-app-dark-img="illustrations/girl-doing-yoga-dark.png" data-app-light-img="illustrations/girl-doing-yoga-light.png" />
+			<div class="text-center">
+				<img src="{{ url('/images/logo_wo_tagline_2.png') }}" class="img-fluid" style="width: 25%;">
+			</div>
+			<Br>
+          	<img src="{{ url('/images/under_maintenance.png') }}" class="img-fluid" width="600" />
           	<h3 class="fw-semibold">Coming Back Soon</h3>
           	<p style="padding: 0px 200px">AYA Bank website is temporarily unavailable and we are working diligently to upgrade the page you requested.</p>
-          	<p>We will be back on 28th June (Wednesday) 6:00AM MMT.</p>
-          	<p>You will find our new facelift.</p>
-          	<p>Follow us on LinkedIn or Facebook for updates.</p>
-          	<p>We apologize for any inconvenience.</p>
+          	<p>We will be back on 28th June (Wednesday) 6:00AM MMT, You will find our new facelift.</p>
+          	<p>Follow us on <a class="text-decoration-underline" href="https://www.linkedin.com/company/ayabank/">LinkedIn</a> or <a class="text-decoration-underline" href="https://www.facebook.com/ayabank">Facebook</a> for updates, We apologize for any inconvenience.</p>
         </div>
 	</div>
 </div>
