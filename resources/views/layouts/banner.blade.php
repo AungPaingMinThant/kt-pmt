d44621fe03971bc214127d954f5043a87d34c9b3
maykhaing-saltnpixel <maykhaing@saltnpixel.com>
2023-08-28 09:36:03 +0630

    GH - fix

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -4040,14 +4040,14 @@
 			<div class="row d-none d-sm-block">
 				<div class="col-md-12 text-left" style="text-align: left;position: relative;">
 					<div class="grievance_handling_banner_tagline">
-						<h1 style="font-size: 23px;font-weight: 400;margin-bottom: 0;line-height: 30px;">We are here <span class="theme_text_color">to help you</span></h1>
+						<h1 style="font-size: 23px;font-weight: 400;margin-bottom: 0;line-height: 30px;">Grievance Handling</h1>
 					</div>
 				</div>
 			</div>
 			<div class="row d-block d-sm-none">
 				<div class="col-md-12 text-left" style="text-align: left;position: relative;">
 					<div style="position: absolute;top: 20px;left: 20px;right: 90px;">
-						<h1 style="font-size: 18px;font-weight: 400;margin-bottom: 0;line-height: 20px;">We are here <br><span class="theme_text_color">to help you</span></h1>
+						<h1 style="font-size: 18px;font-weight: 400;margin-bottom: 0;line-height: 20px;">Grievance Handling</h1>
 					</div>
 				</div>
 			</div>
