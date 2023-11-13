9b9e3108dcdee86207b5a0eb949c01826f77a146
Salt Developer <developer@saltnpixel.com>
2023-11-13 13:01:08 +0630

    pmt

--- app/Http/Controllers/Admin/CreateMemberController.php
+++ app/Http/Controllers/Admin/CreateMemberController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Auth; 
 use App\Models\Member;
@@ -26,32 +28,33 @@ class CreateMemberController extends Controller
     public function addMember(Request $request)
     {
         $id = $request->id;
-        $employee_id = $request->employee_id;
         $name = $request->name;
         $phone = $request->phone;
         $amount = $request->amount;
         $member_point = $request->member_point;
-
+    
         $pointsToAdd = floor($amount / 5000);
         $member_point += $pointsToAdd;
-
+    
+        $latestEmployeeID = Cache::get('last_generated_number', 1000);
+        $latestEmployeeID += 1;
+        Cache::put('last_generated_number', $latestEmployeeID, now()->addDay());
+        $employee_id = "KT_M_" . $latestEmployeeID;
+    
         $member = new Member;
         $member->employee_id = $employee_id;
         $member->name = $name;
         $member->phone = $phone;
         $member->amount = $amount;
         $member->member_point = $member_point;
-        
+    
         $member->created_by = auth()->user()->id;
         $member->updated_by = auth()->user()->id;
         $member->save();
         
-        $member_list = DB::table('members')->get();
-        return view('admin.create.list', ['employee_id' => $employee_id, 'member_list' => $member_list])
-        ->with('success', 'Member added successfully.');
+        $member_list = DB::table('members')->get(); 
+        return view('admin.create.list', ['employee_id' => $employee_id])
+            ->with('success', 'Member added successfully.');
     }
-    private function generateRandomEmployeeID()
-    {
-        return 'KT_M_' . uniqid();
-    }
+    
 }
